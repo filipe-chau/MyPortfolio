cc99499e5cac093c7c94637fdae67c619fd8d06c
filipe-chau <filipealexchau@gmail.com>
2023-05-11 11:13:20 +0200

    modified the code to make darkmode default and light mode an alternative

--- script/main.js
+++ script/main.js
@@ -19,11 +19,11 @@ window.onscroll = () => {
 let darkmode = document.querySelector("#darkmode");
 
 darkmode.onclick = () => {
-    if(darkmode.classList.contains("bx-moon")) {
-        darkmode.classList.replace("bx-moon", "bx-sun");
+    if(darkmode.classList.contains("bx-sun")) {
+        darkmode.classList.replace("bx-sun", "bx-moon");
         document.body.classList.add("active");
     } else {
-        darkmode.classList.replace("bx-sun", "bx-moon");
+        darkmode.classList.replace("bx-moon", "bx-sun");
         document.body.classList.remove("active");
     }
 };
